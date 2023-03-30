2f612eadf8089808fde86a60b558415187e7aa3e
QiroNT <i@ntz.im>
2023-03-30 20:30:29 +0800

    pull web-ext

--- web-ext/src/logic/storage.ts
+++
@@ -1,3 +0,0 @@
-import { useStorageLocal } from '@/composables/useStorageLocal'
-
-export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo')
--- web-ext/src/tests/demo.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from 'vitest'
-
-describe('Demo', () => {
-  it('should work', () => {
-    expect(1 + 1).toBe(2)
-  })
-})
